bd59060b0b82f0cd6c12ea577cb75ad14b5f2f2f
علی یوسفی پور <ali.yousefipour@gmail.com>
2026-09-03 08:49:04 +0330

    Add KhatYar radio native package

---
+++ mobile/plugins/khatyar-radio-native/KhatyarRadioPackage.java
@@ -0,0 +1,20 @@
+package ir.mashhad.taxicontrol.radio;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public final class KhatyarRadioPackage implements ReactPackage {
+  @Override public List<NativeModule> createNativeModules(ReactApplicationContext context) {
+    List<NativeModule> modules = new ArrayList<>();
+    modules.add(new KhatyarRadioModule(context));
+    return modules;
+  }
+  @Override public List<ViewManager> createViewManagers(ReactApplicationContext context) {
+    return Collections.emptyList();
+  }
+}
